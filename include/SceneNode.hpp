d5090b05d334f29ca23c0c71440e6842379a8231
Thinh Vo Truong <thinhhochanh2003@gmail.com>
2023-03-27 00:25:24 +0700

    he

---
+++ include/SceneNode.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+class SceneNode
+{
+public:
+    typedef std::unique_ptr<SceneNode> Ptr;
+
+public:
+    SceneNode();
+
+private:
+    std::vector<Ptr> mChildren;
+    SceneNode *mParent;
+};
