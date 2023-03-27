1b825380b65275cad76f8a17437f065d4da40aed
Vo Truong Thinh <thinhhochanh2003@gmail.com>
2023-03-27 17:50:39 +0700

    Done basic tree operation of our scene graph (page 75)

--- src/SceneNode.cpp
+++ src/SceneNode.cpp
@@ -4,11 +4,20 @@ SceneNode::SceneNode() : mParent(nullptr)
 {
 }
 
-void SceneNode::attachChild(Ptr child){
+void SceneNode::attachChild(Ptr child)
+{
     child->mParent = this;
     mChildren.push_back(std::move(child));
 }
 
-auto SceneNode::detachChild(const SceneNode& node) -> Ptr{
-    
+auto SceneNode::detachChild(const SceneNode &node) -> Ptr
+{
+    auto found = std::find_if(mChildren.begin(), mChildren.end(), [&](Ptr &p) -> bool
+                              { return p.get() == &node; }); // check the element's pointer p.get() == address of the node or not
+    assert(found != mChildren.end());
+
+    auto result = std::move(*found);
+    result->mParent = nullptr;
+    mChildren.erase(found); // erase iterator
+    return result; // return unique_ptr
 }
